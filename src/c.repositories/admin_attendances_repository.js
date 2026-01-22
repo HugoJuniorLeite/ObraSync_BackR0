4e19b2617d49cb4016ce4ef7f373efb709dd4317
Hugo Junior <hjunioreng@gmail.com>
2026-01-21 21:56:53 -0300

    deploy

--- src/c.repositories/admin_attendances_repository.js
+++ src/c.repositories/admin_attendances_repository.js
@@ -8,13 +8,24 @@ async function create_admin_attendance_repository(data) {
 }
 
 async function get_all_admin_attendances_repository(where) {
-  return prisma.admin_attendance.findMany({
+  return prisma.mobile_attendance.findMany({
     where,
     orderBy: {
       attendance_date: "desc",
     },
+    select: {
+      attendance_date: true,
+      technician_name: true,
+      os_number: true,
+      note_number: true,
+      address: true,
+      status: true,
+      deslocamento_inicio: true,
+      finalizado_em: true,
+    },
   });
 }
+
 
 
 async function get_admin_attendance_by_id_repository(attendance_id) {
